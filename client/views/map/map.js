38f100b7857f1bd4183f09460b94c7180d1af5bf
vicnala <vicnala@gmail.com>
2014-03-28 22:19:48 +0100

    Correctly manage the user live marker. Maybe a usable version but  with unlimited user marker creation and poor online user status management.

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,3 +1,5 @@
+var liveMarkerId;
+
 Template.mapMarkers.rendered = function() {
 	var intervalId;
 
@@ -12,53 +14,40 @@ Template.mapMarkers.rendered = function() {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
   }).addTo(window.map);
 
-
-
   // Set the view of the map constantly centered in the user location
 	Deps.autorun(function () {
 		var location = Session.get('location');
-
 		if (location) {
 			if(Meteor.userId()){
-	      var myMarkerId = Session.get('myMarkerId');
-
-	      if (!myMarkerId){
+	      //var liveMarkerId = Session.get('liveMarkerId');
+	      if (!liveMarkerId){
 	        // remove the temp map marker
-	        var key, layers, val;
-	        layers = window.map._layers;
-	        for (key in layers) {
-	          val = layers[key];
-	          if (val.options._id === 'tempId') {
-	            window.map.removeLayer(val);
-	          }
-	        }
-
+					removeMapTempMarker();
 	        // insert a new marker
-	        var newMarkerId = Markers.insert({
-	          nick: 'me!',
-	          message: 'Hi all!',
-	          location: [location[0], location[1]],
-	          public: true,
-	          live: true,
-	          submitted: new Date().getTime()
-	        });
-
-	        Session.set('myMarkerId', newMarkerId);
-
+	        liveMarkerId = Markers.insert(defaultMarker(location));
+	        // set the 'liveMarkerId' session variable
+	        Session.set('liveMarkerId', liveMarkerId);
+	        // Set the user profile variable 'liveMarker'
+	        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.liveMarker": liveMarkerId}})
 	        // Setup remote keepalive call
-	        Meteor.call('keepalive', newMarkerId);
+	        Meteor.call('keepalive', liveMarkerId);
 	        intervalId = Meteor.setInterval(function () {
-	          Meteor.call('keepalive', Session.get('myMarkerId'));
+	          Meteor.call('keepalive', Session.get('liveMarkerId'));
 	        }, 10000);
+
+	        // enable double click to place markers
+	        enabledbclick();
 	      }
 	      else {
 	        // update my marker
-	        Markers.update(myMarkerId, {$set: {location: location}});
+	        Markers.update(liveMarkerId, {$set: {location: location}});
 	      }
         // center the map
         window.map.setView([location[1], location[0]]);
 			}
 			else {
+				// Clear Session variable liveMarkerId
+				delete Session.keys['liveMarkerId'];
 				// clear keepalive timer
 				if (intervalId) {
 					Meteor.clearInterval(intervalId);
@@ -66,51 +55,21 @@ Template.mapMarkers.rendered = function() {
 
 				// TODO: delete the database marker (to make disappear the map marker instantaneously)
 
-				// setup a temp map marker
-	      var marker = L.marker(
-	        {lon: location[0], lat: location[1]},
-	        {
-	          title: 'me!',
-	          _id: 'tempId',
-	          icon: createIcon('me!', 'blue')
-	        }
-	      ).addTo(window.map).bindPopup(
-	        '<b>Wellcome to <em>mapea.me!</em></b><br>Login to start using this <em>amazing</em> app!'
-	      ).openPopup();
-
+				// disable dblclick
+				window.map.off('dblclick', null);
+				// setup a new temp map marker
+				removeMapTempMarker();
+				defaultMapMarker(location);
 				// center the map
 				window.map.setView([location[1], location[0]]);
 			}
 		}
 	});
 
-
-
-	// Enable double click marker insert for logged in user
-	Deps.autorun(function(){
-	  if(Meteor.userId()){
-		  window.map.on('dblclick', function(e) {
-		    Markers.insert({
-		      nick: 'new!',
-		      message: 'Hi all!',
-		      location: [e.latlng.lng, e.latlng.lat],
-		      public: true,
-		      submitted: new Date().getTime()
-		    });
-		  });
-	  }
-	  else {
-	  	window.map.off('dblclick', null);
-	  }
-	});
-
-
-
 	// Obderve the markers collection to add/change/remove the map markers
   Markers.find({}).observe({
     added: function(mark) {
       var marker;
-      //console.log(mark);
       marker = L.marker(
         {lon: mark.location[0], lat: mark.location[1]},
         {
@@ -121,13 +80,11 @@ Template.mapMarkers.rendered = function() {
       ).addTo(window.map).bindPopup(
         '<b>' + mark.nick + '</b><br>' + mark.message + '<br><a href="/markers/' + mark._id + '" class="discuss btn">Comment</a>'
       );
-
       return marker;
     },
 
     changed: function(mark) {
       var key, layers, val, _results;
-      var id = Session.get('myMarkerId');
       layers = window.map._layers;
       _results = [];
       for (key in layers) {
@@ -135,7 +92,6 @@ Template.mapMarkers.rendered = function() {
         if (!val._latlng) {
 
         } else {
-          //console.log('val', val);
           if (val.options._id === mark._id){
             console.log('this is the updated marker');
             val.options.title = mark.nick;
@@ -144,7 +100,7 @@ Template.mapMarkers.rendered = function() {
 
             // TODO: update .bindPopup()
 
-            if (val.options._id === id) {
+            if (val.options._id === liveMarkerId) {
               console.log('  and has my _id: blue and pan');
               val.setIcon(createIcon(mark.nick, 'blue'));
               window.map.setView([mark.location[1], mark.location[0]], 17);
@@ -157,7 +113,7 @@ Template.mapMarkers.rendered = function() {
           }
           else {
             console.log('this is NOT the updated marker');
-            if (val.options._id === id) {
+            if (val.options._id === liveMarkerId) {
               //do nothing
             }
             else {
@@ -194,5 +150,72 @@ var createIcon = function(nick, color) {
     iconSize: [10, 10],
     html: '<b>' + nick + '</b>',
     className: className  
+  });
+}
+
+
+var removeMapTempMarker = function() {
+  var key, layers, val;
+  layers = window.map._layers;
+  for (key in layers) {
+    val = layers[key];
+    if (val.options._id === 'tempId') {
+  		window.map.removeLayer(val);
+    }
+  }
+}
+
+var defaultMarker = function(location) {
+	return {
+		nick: 'me!',
+		message: 'Hi all!',
+		location: [location[0], location[1]],
+		public: true,
+		live: true,
+		submitted: new Date().getTime()
+	};
+}
+
+
+var defaultMapMarker = function(location) {
+  L.marker(
+    {lon: location[0], lat: location[1]},
+    {
+      title: 'me!',
+      _id: 'tempId',
+      icon: createIcon('me!', 'blue')
+    }
+  ).addTo(window.map).bindPopup(
+    '<b>Wellcome to <em>mapea.me!</em></b><br>Sing up to start using this <em>amazing</em> app!'
+  ).openPopup();
+}
+
+var defaultMarker = function(location) {
+	return {
+		nick: 'me!',
+		message: 'Hi all!',
+		location: [location[0], location[1]],
+		public: true,
+		live: true,
+		submitted: new Date().getTime()
+	};
+}
+
+var clicks = 0;
+
+var enabledbclick = function(location) {
+  // enable double click marker insert for logged in user
+  window.map.on('dblclick', function(e) {
+  	if(Meteor.userId()){
+  		clicks = clicks + 1;
+  		console.log(clicks);
+	    Markers.insert({
+	      nick: 'new!',
+	      message: 'Hi all!',
+	      location: [e.latlng.lng, e.latlng.lat],
+	      public: true,
+	      submitted: new Date().getTime()
+	    });
+	  }
   });
 }
